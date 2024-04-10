b505deb6e4f6ffae83471496439dc6d32cd2756b
takaodaze <gin2798@gmail.com>
2024-04-10 22:30:37 +0900

    install prettier

--- App.tsx
+++ App.tsx
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
-export default function App () {
+export default function App() {
+  const [length, setLength] = useState(1);
+
+  const handleChange = (text: string) => {
+    const maybeInt = parseInt(text);
+    if (isNaN(maybeInt)) {
+      setLength(0);
+      return;
+    }
+    setLength(maybeInt);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <TextInput
+        value={length.toString()}
+        onChangeText={(t) => {
+          handleChange(t);
+        }}
+      />
+      {new Array(length).fill(null).map((_, i) => (
+        <View key={`sample-${i}`}>
+          <Text>Hello World!</Text>
+        </View>
+      ))}
       <StatusBar style="auto" />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
